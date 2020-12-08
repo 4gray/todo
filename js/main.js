3a380715ae55ed93995f71001984acf5e39db2e7
4gray <serega05@gmail.com>
2020-12-08 19:53:35 +0100

    refactor: switch to the class construct in main.js

--- js/main.js
+++ js/main.js
@@ -1,56 +1,62 @@
 import ToDoList from "./todolist.js";
 import ToDoItem from "./todoitem.js";
 
-const toDoList = new ToDoList();
-
 // Launch app
-
 document.addEventListener("readystatechange", (event) => {
     if (event.target.readyState === "complete") {
-        initApp();
+        window.TODO_APP = new App();
     }
 })
 
-const initApp = () => {
-    // Listeners
+export default class App {
 
-    //Procedural
+    toDoList;
 
-    //Load List Object
-
-    //refresh page
-    refreshPage();
-}
-
-const refreshPage = () => {
-    clearListDisplay();
-    //renderList();
-    //clearItemEntryField();
-    //SetFocusOnItemEntry();
-}
-
-const clearListDisplay = () => {
-    const parentElement = document.getElementById("listItems");
-    deleteContects(parentElement);
-}
-
-const deleteContects = (parentElement) => {
-    let child = parentElement.lastElementChild;
-    while(child) {
-        parentElement.removeChild(child);
-        child = parentElement.lastElementChild;
+    constructor() {
+        this.toDoList = new ToDoList();
     }
-}
 
-const renderList = () => {
-    const list = toDoList.getList();
-    list.forEach(
-        (item) => {
-            buildListItem(item);
+    initApp() {
+        // Listeners
+
+        //Procedural
+
+        //Load List Object
+
+        //refresh page
+        this.refreshPage();
+    }
+
+    refreshPage() {
+        this.clearListDisplay();
+        //renderList();
+        //clearItemEntryField();
+        //SetFocusOnItemEntry();
+    }
+
+    clearListDisplay() {
+        const parentElement = document.getElementById("listItems");
+        this.deleteContects(parentElement);
+    }
+
+    deleteContects(parentElement) {
+        let child = parentElement.lastElementChild;
+        while(child) {
+            parentElement.removeChild(child);
+            child = parentElement.lastElementChild;
         }
-    );
-}
+    }
 
-const buildListItem = (item) => {
-    
+    renderList() {
+        const list = toDoList.getList();
+        list.forEach(
+            (item) => {
+                buildListItem(item);
+            }
+        );
+    }
+
+    buildListItem(item) {
+
+    }
 }
